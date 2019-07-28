ec98e91a60d355b9d0453df891809b6e87ab7d32
manpreetc90 <machopra.work10@gmail.com>
2019-07-28 22:17:45 +0530

    Best practises: created ToDo class

--- frontend/todo/src/app/list-todos/list-todos.component.ts
+++ frontend/todo/src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+
+export class Todo {
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ) {
+
+  }
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -7,9 +19,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListTodosComponent implements OnInit {
 
-  todos = [{
-    id: 1, description: 'Daily call'
-  }];
+  todos = [
+     new Todo(1, 'Daily Call', false, new Date()),
+     new Todo(2, 'Work on defects', false, new Date()),
+     new Todo(3, 'Check Emails', false, new Date())
+  ];
   constructor() { }
 
   ngOnInit() {
