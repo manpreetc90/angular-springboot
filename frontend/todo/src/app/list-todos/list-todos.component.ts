cca08165700eb865afcf044af3d1bc53e2c8621d
manpreetc90 <machopra.work10@gmail.com>
2019-07-28 21:30:10 +0530

    created list to dos component with static todos

---
+++ frontend/todo/src/app/list-todos/list-todos.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-todos',
+  templateUrl: './list-todos.component.html',
+  styleUrls: ['./list-todos.component.css']
+})
+export class ListTodosComponent implements OnInit {
+
+  todos = [{
+    id: 1, description: 'Daily call'
+  }];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
