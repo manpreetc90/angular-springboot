d39e650cb7344ab1db784ec2761d87cff2097454
manpreetc90 <machopra.work10@gmail.com>
2019-07-30 21:24:13 +0530

    Using Session Storage to Store User Authencation Token

--- frontend/todo/src/app/login/login.component.ts
+++ frontend/todo/src/app/login/login.component.ts
@@ -17,11 +17,18 @@ export class LoginComponent implements OnInit {
     //console.log(this.username);
     if(this.hardcodedAuthenticationService.authenticate(this.username, this.password)){
       this.router.navigate(['welcome', this.username]);
+      sessionStorage.setItem('authenticatedUser', this.username);
       this.invalidLogin = false;
     } else {
       this.invalidLogin = true;
     }
 
+  }
+
+  isUserLoggedIn(){
+    // tslint:disable-next-line: prefer-const
+    let user = sessionStorage.getItem('authenticatedUser');
+    return !(user === null);
   }
   constructor(private router: Router,
               private hardcodedAuthenticationService : HardcodedAuthenticationService) { }
