12da2f320d64de16e356aea447037d00ecffccf9
manpreetc90 <machopra.work10@gmail.com>
2019-08-05 23:29:56 +0530

    Adding HelloWorld controller

---
+++ backend/restful-web-services/src/main/java/com/manpreet/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.manpreet.rest.webservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
+public class HelloWorldController {
+	
+	//GET
+	//URI -- /hello-world
+	// return method - "Hello World"
+	//@RequestMapping(method=RequestMethod.GET, path="/hello-world")
+	@GetMapping(path="/hello-world")
+	public String helloWorld(){
+		return "Hello World";
+	}
+
+}
