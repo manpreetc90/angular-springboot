536fa2727d1161f90b091d24ba0d20aedd41f846
manpreetc90 <machopra.work10@gmail.com>
2019-08-11 22:44:38 +0530

    Connecting Data Service: created service

--- frontend/todo/src/app/welcome/welcome.component.ts
+++ frontend/todo/src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -10,12 +11,19 @@ export class WelcomeComponent implements OnInit {
 
   name = '';
   //ActivatedRoute
-  constructor(private route: ActivatedRoute) {
+  constructor(
+    private route: ActivatedRoute,
+    private service: WelcomeDataService) {
 
    }
 
   ngOnInit() {
     this.name = this.route.snapshot.params['name'];
+  }
+
+  getWelcomeMessage(){
+  //    console.log('Get Welcome Message');
+    this.service.executeHelloWorldBeanService();
   }
 
 }
